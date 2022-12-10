66b7df19e7c79718cda5af4859dd2fbb6e43cde8
hsallrounder <hsallrounder@gmail.com>
2022-12-10 15:22:05 +0530

    you have won the game

--- imageMatch.js
+++ imageMatch.js
@@ -3,6 +3,7 @@ const cards = document.querySelectorAll('.image-card');
 let hasFlippedCard = false;
 let lockBoard = false;
 let card1, card2;
+let pairs_count=0;
 
 function cardFlip() {
   if (lockBoard) return;
@@ -20,28 +21,31 @@ function cardFlip() {
 
   // second click
   card2 = this;
-  var value=document.getElementById('trials').innerHTML;
-  var new_count=(value.charAt(value.length - 1)-'0') - 1;
-  var new_value=value.substring(0, value.length - 1)+new_count;
-  document.getElementById('trials').innerHTML=new_value;
-  match_check();
-  if(checkfortrials()){
-    alert("Game Over");
-    window.location.reload();
+  if(!match_check()){
+    var value=document.getElementById('trials').innerHTML;
+    var new_count=(value.charAt(value.length - 1)-'0') - 1;
+    var new_value=value.substring(0, value.length - 1)+new_count;
+    document.getElementById('trials').innerHTML=new_value;
+    if(checkfortrials()){
+      pairs_count=0;
+      alert("Game Over");
+      window.location.reload();
+    }
   }
+  if(pairs_count==6) alert("You Have Won The Game")
 }
 
 function checkfortrials(){
   var value=document.getElementById('trials').innerHTML;
   var count=value.charAt(value.length - 1)-'0';
-  console.log(count==0);
   return count==0;
 }
 
 function match_check() {
   let isMatch = card1.dataset.framework === card2.dataset.framework;
-
+  if(isMatch) pairs_count++;
   isMatch ? disableCards() : uncardFlips();
+  return isMatch;
 }
 
 function disableCards() {
